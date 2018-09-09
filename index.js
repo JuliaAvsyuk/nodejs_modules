0067831334e9ad2ef1b99732e8f69cf6f9f06a03
ЮЛЯ-ПК\ЮЛЯ <julia.avsyuk@mail.ru>
2018-09-09 15:57:16 +0300

    no message

--- index.js
+++ index.js
@@ -16,11 +16,15 @@ let http = require("http");
 http.createServer(function (request, response) {
        response.end("Hello WOrld!!");
 }).listen(3000, "127.0.0.1", function () {
-    console.log("Server starts");
 });
+
+let newName = require("./newName");
+let newname = new User("Jessy", 20);
+console.log(`Hello, ${newname.name}. Are you ${newname.age} years old?`);
 //D:\Programming\Training\трен_1\nodejs_modules>node index.js
 //Name: Julia Age: 25
 //Hello! My name is Julia. I'm 25 years old. Nice to meet you!
 //Дата запроса: Sun Sep 09 2018 15:33:19 GMT+0300 (Беларусь (зима))
 //Good afretnoon, ЮЛЯ
-//Server starts
+// Hello, Jessy. Are you 20 years old?
+
